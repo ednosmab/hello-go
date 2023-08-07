eb9dc8e95e7ab4ccf80d0e323059ab234c47e88a
Edson Garcia <ednosmab@yahoo.com.br>
2023-08-07 17:07:54 -0300

    Importing os, implementing status code

--- hello.go
+++ hello.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
@@ -49,7 +50,9 @@ func solicitaOpcao() {
 		fmt.Println("Exibindo logs...")
 	case 0:
 		fmt.Println("Saindo do programa")
+		os.Exit(0)
 	default:
 		fmt.Println("Não conheço este comando!")
+		os.Exit(-1)
 	}
 }
