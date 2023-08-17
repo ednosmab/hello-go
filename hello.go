0f777ab7eef680272e7e9f1a23627a5383f67f37
Edson Garcia <ednosmab@yahoo.com.br>
2023-08-17 11:53:44 -0300

    working with external files

--- hello.go
+++ hello.go
@@ -1,36 +1,44 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
-const monitorings = 5
+const monitoring = 5
 const delay = 3
 const delayMessage = 1
 
 func main() {
-	leArquivoExterno()
-	// name := requestName()
-	// displayIntroduction(name)
-	// for {
-	// 	displayOptions(name)
 
-	// 	requestOption()
-	// }
+	name := requestName()
+
+	displayIntroduction(name)
+
+	for {
+		displayOptions(name)
+
+		requestOption()
+	}
 }
 
 func informName() {
 	fmt.Println("Informe o seu nome")
 }
+
 func requestName() string {
 	var name string
 	informName()
 	fmt.Scan(&name)
+
 	return name
 }
+
 func displayIntroduction(name string) {
 	version := 1.1
 	fmt.Println("Bem vindo ao Site Monitor,", name)
@@ -47,11 +55,13 @@ func displayOptions(name string) {
 func informOption() int {
 	var comando int
 	fmt.Scan(&comando)
+
 	return comando
 }
 
 func requestOption() {
 	command := informOption()
+
 	switch command {
 	case 1:
 		startMonitoring()
@@ -70,63 +80,85 @@ func requestSite() string {
 	fmt.Println("Informe o site a ser verificado:")
 	var site string
 	fmt.Scan(&site)
+
 	return site
 }
+
 func getStatusCode(site string) int {
+	site = "https://" + site
 	resp, err := http.Get(site)
+
 	if err != nil {
 		fmt.Println(err)
-		return -1
+		return 0
 	}
 	return resp.StatusCode
-}
-
-func allSites() []string {
-	sites := []string{
-		"https://www.google.com.br",
-		"https://www.casadocodigo.com.br/",
-		"http://alura.com.br/",
-	}
-
-	return sites
 }
 
 func siteValidate(site string) {
 	if getStatusCode(site) != 200 {
 		fmt.Println("Site verificado:", site, "Não foi carregado com sucesso")
+	} else {
+		fmt.Println("Site verificado:", site)
+		fmt.Println("Status: carregado com sucesso!")
 	}
-	fmt.Println("Site verificado:", site)
-	fmt.Println("Status: carregado com sucesso!")
+
 }
+
 func processSite() {
-	sites := allSites()
-	for i := 0; i < monitorings; i++ {
+	sites := readExternalFile()
+	fmt.Println(sites)
+	for i := 0; i < monitoring; i++ {
+		if len(sites) == 0 {
+			break
+		}
 		for _, site := range sites {
 			siteValidate(site)
 		}
 		fmt.Println("")
 		time.Sleep(delay * time.Second)
 	}
+
 	fmt.Println("-----------Menu de opções------------")
 }
+
 func startMessage() {
 	fmt.Println("")
 	fmt.Print("Monitorando ")
+
 	for i := 0; i < 3; i++ {
 		fmt.Print(".")
 		time.Sleep(delayMessage * time.Second)
 	}
 }
+
+func readExternalFile() []string {
+	lines := []string{}
+	file, err := os.Open("sites.txt")
+	reader := bufio.NewReader(file)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro: ", err)
+		return lines
+	}
+
+	for {
+		line, err := reader.ReadString('\n')
+		line = strings.TrimSpace(line)
+
+		lines = append(lines, line)
+
+		if err == io.EOF {
+			break
+		}
+
+	}
+
+	file.Close()
+	return lines
+}
+
 func startMonitoring() {
 	startMessage()
 	processSite()
-}
-
-func leArquivoExterno() {
-	arquivo, err := os.Open("sites.txt")
-	if err != nil {
-		fmt.Println("Ocorreu um erro: ", err)
-		return
-	}
-	fmt.Println(arquivo)
 }
